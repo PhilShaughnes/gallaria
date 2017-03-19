fe237a93be3992449a6e545790b8b15c164610c2
Phil <Philip.Shaughnessy@gmail.com>
2017-03-19 19:18:07 -0400

    fixed links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
 
   before_action :find_user, only: [:show]
 
+  def index
+    @users = User.all
+  end
 
   def show
     @galleries = @user.galleries
