9d011e54f25cdba8a89e69dae82c5c76a041ac36
Phil <Philip.Shaughnessy@gmail.com>
2017-03-20 02:13:17 -0400

    added heroku url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { :host => "https://secret-cliffs-40919.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "https://serene-everglades-80828.herokuapp.com/" }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
