3b9763596c5ee490b996909ba337a28a5f04044d
Phil <Philip.Shaughnessy@gmail.com>
2017-03-20 23:49:45 -0400

    fixed share button problems

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,7 +1,6 @@
 class SharesController < ApplicationController
 
   def create
-
     @object = params[:photo_id] ? Photo.find(params[:photo_id]) : Gallery.find(params[:gallery_id])
 
     UserMailer.share(@object, params[:share][:email]).deliver
