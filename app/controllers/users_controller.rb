a909c3d96b6b586a37cc65bc680497f09a99242c
Phil <Philip.Shaughnessy@gmail.com>
2017-03-20 20:57:57 -0400

    fixed user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     if @user.save
       UserMailer.signup(@user).deliver
       session[:user_id] = @user.id
-      redirect_to user_path
+      redirect_to user_path(@user)
     else
       render :new
     end
